e43431eb5e12a94dc0075970a1e3ae456f334fae
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-28 01:48:03 +0200

    mobile: add MVC model for Android scenario

---
+++ appium-android-tests/src/test/java/com/king_sparkon_tracker/qa/android/model/AndroidScenarioModel.java
@@ -0,0 +1,9 @@
+package com.king_sparkon_tracker.qa.android.model;
+
+public record AndroidScenarioModel(
+        String id,
+        String scenario,
+        String name,
+        String description
+) {
+}
