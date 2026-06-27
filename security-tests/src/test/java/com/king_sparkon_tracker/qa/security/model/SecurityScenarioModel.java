694a62af824e97e9c136320696c14bb8dc9a59bb
Sizolwakhe Leonard Mthimunye <leonard1thecoder@gmail.com>
2026-06-28 01:49:05 +0200

    security: add MVC model for security scenario

---
+++ security-tests/src/test/java/com/king_sparkon_tracker/qa/security/model/SecurityScenarioModel.java
@@ -0,0 +1,9 @@
+package com.king_sparkon_tracker.qa.security.model;
+
+public record SecurityScenarioModel(
+        String id,
+        String scenario,
+        String name,
+        String description
+) {
+}
